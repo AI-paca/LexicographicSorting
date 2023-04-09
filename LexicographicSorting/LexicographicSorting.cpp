837a1cefddd22a15ac22693f9ea790edcb3d86d9
AI-paca <121185398+AI-paca@users.noreply.github.com>
2023-04-09 15:36:47 +0400

    Update LexicographicSorting.cpp

--- LexicographicSorting/LexicographicSorting.cpp
+++ LexicographicSorting/LexicographicSorting.cpp
@@ -1,137 +1,105 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
-/*Ввести с клавиатуры произвольное абстрактное предложение длиной не более 80 символов,
-состоящее из абстрактных слов. Слова в предложении отделяются друг от друга как минимум
-одним символом «пробел». Предложение должно быть введено с помощью функции getchar().
-Используя введённое предложение, построить новое предложение, в котором слова упорядочены
-по возрастанию в лексикографическом порядке*/
+/*Р’РІРµСЃС‚Рё СЃ РєР»Р°РІРёР°С‚СѓСЂС‹ РїСЂРѕРёР·РІРѕР»СЊРЅРѕРµ Р°Р±СЃС‚СЂР°РєС‚РЅРѕРµ РїСЂРµРґР»РѕР¶РµРЅРёРµ РґР»РёРЅРѕР№ РЅРµ Р±РѕР»РµРµ 80 СЃРёРјРІРѕР»РѕРІ,
+СЃРѕСЃС‚РѕСЏС‰РµРµ РёР· Р°Р±СЃС‚СЂР°РєС‚РЅС‹С… СЃР»РѕРІ. РЎР»РѕРІР° РІ РїСЂРµРґР»РѕР¶РµРЅРёРё РѕС‚РґРµР»СЏСЋС‚СЃСЏ РґСЂСѓРі РѕС‚ РґСЂСѓРіР° РєР°Рє РјРёРЅРёРјСѓРј
+РѕРґРЅРёРј СЃРёРјРІРѕР»РѕРј В«РїСЂРѕР±РµР»В». РџСЂРµРґР»РѕР¶РµРЅРёРµ РґРѕР»Р¶РЅРѕ Р±С‹С‚СЊ РІРІРµРґРµРЅРѕ СЃ РїРѕРјРѕС‰СЊСЋ С„СѓРЅРєС†РёРё getchar().
+РСЃРїРѕР»СЊР·СѓСЏ РІРІРµРґС‘РЅРЅРѕРµ РїСЂРµРґР»РѕР¶РµРЅРёРµ, РїРѕСЃС‚СЂРѕРёС‚СЊ РЅРѕРІРѕРµ РїСЂРµРґР»РѕР¶РµРЅРёРµ, РІ РєРѕС‚РѕСЂРѕРј СЃР»РѕРІР° СѓРїРѕСЂСЏРґРѕС‡РµРЅС‹
+РїРѕ РІРѕР·СЂР°СЃС‚Р°РЅРёСЋ РІ Р»РµРєСЃРёРєРѕРіСЂР°С„РёС‡РµСЃРєРѕРј РїРѕСЂСЏРґРєРµ*/
 
 class Sentence {
 public:
-	char* arrchars;//сивмвольная строка
-	char** arrwords;//упорядоченный набор слов 
+    char* arrchars;//СЃРёРІРјРІРѕР»СЊРЅР°СЏ СЃС‚СЂРѕРєР°
+    char** arrwords;//СѓРїРѕСЂСЏРґРѕС‡РµРЅРЅС‹Р№ РЅР°Р±РѕСЂ СЃР»РѕРІ 
 
-	class Sentence() //создать абстрактное предложение
-	{
-		arrchars = (char*)malloc(81 * sizeof(char));
-		arrwords = (char**)malloc(40 * sizeof(char*));
-		int count;
-		puts("Введи предложение");
-		for (int i = 0; (i < 80 && arrchars[i - 1] != '\n'); i++) {
-			arrchars[i] = getchar(); count = i;//подсчет длины строки
-		}
-		arrchars = (char*)realloc(arrchars, count * sizeof(char)); arrchars[count] = '\0';
-		char* arrchars = _strdup(this->arrchars);
-		arrwords[0] = strtok(arrchars, " ");
-		for (int i = 1; (i < 40 && arrwords[i - 1] != NULL); i++) {
-			arrwords[i] = strtok(NULL, " "); count = i;//подсчет слов
-		}
-		arrwords = (char**)realloc(arrwords, (count + 1) * sizeof(char*));
-	}
+    class Sentence() //СЃРѕР·РґР°С‚СЊ Р°Р±СЃС‚СЂР°РєС‚РЅРѕРµ РїСЂРµРґР»РѕР¶РµРЅРёРµ
+    {
+        arrchars = (char*)malloc(81 * sizeof(char));
+        arrwords = (char**)malloc(40 * sizeof(char*));
+        int count;
+        puts("Р’РІРµРґРё РїСЂРµРґР»РѕР¶РµРЅРёРµ");
+        for (int i = 0; (i < 80 && arrchars[i - 1] != '\n'); i++) {
+            arrchars[i] = getchar(); count = i;//РїРѕРґСЃС‡РµС‚ РґР»РёРЅС‹ СЃС‚СЂРѕРєРё
+        }
+        count++;
+        arrchars = (char*)realloc(arrchars, count * sizeof(char));
+        arrchars[count - 1] = '\0';
+        char* arrchars = _strdup(this->arrchars);
+        arrwords[0] = strtok(arrchars, " ");
+        for (int i = 1; (i < 40 && arrwords[i - 1] != NULL); i++) {
+            arrwords[i] = strtok(NULL, " "); count = i;//РїРѕРґСЃС‡РµС‚ СЃР»РѕРІ
+        }
+        arrwords = (char**)realloc(arrwords, (count + 1) * sizeof(char*));
+    }
 
-	class Sentence(char** arrwords) //создать строку из слов
-	{
-		arrchars = new char{ '\0' };
-		for (int i = 0; arrwords[i] != NULL; i++) {
-			strcat(arrchars, arrwords[i]);
-			strcat(arrchars, " ");
-		}
-	}
+    class Sentence(char** arrwords) //СЃРѕР·РґР°С‚СЊ СЃС‚СЂРѕРєСѓ РёР· СЃР»РѕРІ
+    {
+        int len = 0;
+        for (int i = 0; arrwords[i] != NULL; i++)
+            len += strlen(arrwords[i]) + 1;
+        arrchars = (char*)malloc((len + 1) * sizeof(char));
+        arrchars[0] = '\0';
+        strcat(arrchars, arrwords[0]);
+        for (int i = 1; arrwords[i] != NULL; i++) {
+            strcat(arrchars, " ");
+            strcat(arrchars, arrwords[i]);
+        }
+    }
 
-	void clear() //удалить предложение
-	{
-		if (arrwords != nullptr) {
-			free(arrwords);
-		}
-		//if (arrchars != NULL && strlen(arrchars) > 0) {
-		//	free(arrchars);
-		//}
-	}
+    void clear() //СѓРґР°Р»РёС‚СЊ РїСЂРµРґР»РѕР¶РµРЅРёРµ
+    {
+        if (arrwords != nullptr) {
+            free(arrwords);
+        }
+        if (arrchars != NULL && strlen(arrchars) > 0) {
+            free(arrchars);
+            arrchars = nullptr;
+        }
+    }
 
-	char** SortByAlfa() { //отсортировать в лексикографическом порядке
-		int lenArrWords = 0; for (int i = 0; arrwords[i] != NULL; i++) { lenArrWords += 1; }
-		int step = lenArrWords / 2;
-		while (step > 0) {
-			for (int i = step; i < lenArrWords; i++)
-			{
-				int i_right = i;
-				int i_left = i_right - step;
-				while (i_left >= 0) {
-					for (size_t j = 0; j <= strlen(arrwords[i_left]); j++) {
-						if (strncmp(arrwords[i_left], arrwords[i_right], j) > 0) {
-							char* temp = arrwords[i_left];
-							arrwords[i_left] = arrwords[i_right];
-							arrwords[i_right] = temp;
-							break;
-						}
-					}
+    char** SortByAlfa() { //РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°С‚СЊ РІ Р»РµРєСЃРёРєРѕРіСЂР°С„РёС‡РµСЃРєРѕРј РїРѕСЂСЏРґРєРµ
+        int lenArrWords = 0; for (int i = 0; arrwords[i] != NULL; i++) { lenArrWords += 1; }
+        int step = lenArrWords / 2;
+        while (step > 0) {
+            for (int i = step; i < lenArrWords; i++)
+            {
+                int i_right = i;
+                int i_left = i_right - step;
+                while (i_left >= 0) {
+                    for (size_t j = 0; j <= strlen(arrwords[i_left]); j++) {
+                        if (strncmp(arrwords[i_left], arrwords[i_right], j) > 0) {
+                            char* temp = arrwords[i_left];
+                            arrwords[i_left] = arrwords[i_right];
+                            arrwords[i_right] = temp;
+                            break;
+                        }
+                    }
 
-					i_right = i_left;
-					i_left = i_right - step;
-				}
-			}
-			step /= 2;
-		}
-		for (int i = 0; (arrwords[i] != NULL); i++) {
-			puts(arrwords[i]);
-		}
-		return arrwords;
-	}
-
-	//сортировка без strncmp
-	//private: int CompearByAlfa(int iL, int iR) //сранвить слова 
-	//    {
-	//        int k = 0;
-	//    start:
-	//        if (arrwords[iL][k] < arrwords[iR][k]) { return 0; }//в лексикографическом
-	//        if (arrwords[iL][k] == arrwords[iR][k]) {
-	//            if (k < strlen(arrwords[iL])) { k += 1; goto start; }
-	//            else if (k == strlen(arrwords[iL]) && k == strlen(arrwords[iR])) { //по возраствнию
-	//                if (strlen(arrwords[iL]) > strlen(arrwords[iR])) { return 0; }
-	//                else { return 1;}
-	//            }
-	//        }
-	//        else { return 1; }
-	//    }
-
-	//public: Sentence SortByAlfa() { //отсортировать в лексикографическом порядке
-	//        int lenArrWords = 0; for (int i = 0; arrwords[i] != NULL; i++) { lenArrWords += 1; }
-	//        int step = lenArrWords / 2;
-	//        while (step > 0) {
-	//            for (int i = step; i < lenArrWords; i++)
-	//            {
-	//                int i_right = i;
-	//                int i_left = i_right - step;
-	//                while (i_left >= 0 and i_left != i_right and i_right < lenArrWords and CompearByAlfa(i_left, i_right) == 1) {
-	//                    char* temp = arrwords[i_left];
-	//                    arrwords[i_left] = arrwords[i_right];
-	//                    arrwords[i_right] = temp;
-	//                    i_right = i_left;
-	//                    i_left = i_right - step;
-	//                }
-	//            }
-	//            step /= 2; 
-	//        }
-	//        /*for (int i = 0; (arrwords[i] != NULL); i++) {
-	//            puts(arrwords[i]);
-	//        }*/
-	//       return Sentence(arrwords);
-	//    }
+                    i_right = i_left;
+                    i_left = i_right - step;
+                }
+            }
+            step /= 2;
+        }
+        for (int i = 0; (arrwords[i] != NULL); i++) {
+            puts(arrwords[i]);
+        }
+        return arrwords;
+    }
 
 };
 
 
 void main()
 {
-	system("chcp 1251");
-	std::cout << "Лабораторная №\n";
-	Sentence sent;
-	std::cout << "\nОтсортированное предложение:\n";
-	Sentence sortsent(sent.SortByAlfa());
-	puts(sortsent.arrchars);
-	std::cout << "Исходное предложение:\n";
-	puts(sent.arrchars);
-	sortsent.clear();
-	sent.clear();
+    system("chcp 1251");
+    std::cout << "Р›Р°Р±РѕСЂР°С‚РѕСЂРЅР°СЏ в„–\n";
+    Sentence sent;
+    std::cout << "\nРћС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅРѕРµ РїСЂРµРґР»РѕР¶РµРЅРёРµ:\n";
+    Sentence sortsent(sent.SortByAlfa());
+    puts(sortsent.arrchars);
+    std::cout << "РСЃС…РѕРґРЅРѕРµ РїСЂРµРґР»РѕР¶РµРЅРёРµ:\n";
+    puts(sent.arrchars);
+    sortsent.clear();
+    sent.clear();
 }
